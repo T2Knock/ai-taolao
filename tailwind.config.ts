5f0d212d82683787553c313168220242b0b6a00b
Valgus <khuenguyenhaminh.com>
2024-07-12 16:35:55 +0700

    style: add theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,19 @@ export default {
     extend: {
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        heading: 'Lemon',
+        body: 'IBM Plex Sans KR',
       },
     },
+    theme: {
+      colors: {
+        'text': '#0d0f12',
+        'background': '#f2f4f7',
+        'primary': '#303d55',
+        'secondary': '#a9b7d1',
+        'accent': '#899fc8',
+       },
+    }
   },
   plugins: [require("daisyui")],
 } satisfies Config;
