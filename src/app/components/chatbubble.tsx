7ebb31c79a34819f2106c36bed570bf59696dc4e
Valgus <khuenguyenhaminh.com>
2024-07-12 16:40:31 +0700

    feat: add chat bubble

---
+++ src/app/components/chatbubble.tsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return (
+    <div>
+<div className="chat chat-start">
+  <div className="chat-bubble">
+    Hello
+    <br />
+    Hi
+  </div>
+</div>
+<div className="chat chat-end">
+  <div className="chat-bubble">You underestimate my power!</div>
+</div>
+    </div>
+  );
+}
