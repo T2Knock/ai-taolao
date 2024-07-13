5477147e0c7357da9169af3755cf5ab00e65d4f1
knock-huynh <huynhtransongngoc@gmail.com>
2024-07-13 15:17:55 +0700

    chore: add fastify-plugin fastify autoload

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,1 +1,25 @@
-console.log("what the heck bro");
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", () => {
+  return { hello: "world" };
+});
+
+/**
+ * Run the server!
+ */
+const start = async (): Promise<void> => {
+  try {
+    await fastify.listen({ port: 3000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start().catch((error) => {
+  fastify.log.error(error);
+});
