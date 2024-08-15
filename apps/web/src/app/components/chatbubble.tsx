6f020445c10820b7db5aa764d2fd94dccf5ed1dc
Valgus <khuenguyenhaminh.com>
2024-08-15 16:15:17 +0700

    feat: add conversation

--- apps/web/src/app/components/chatbubble.tsx
+++ apps/web/src/app/components/chatbubble.tsx
@@ -1,17 +1,17 @@
-const Conversation = () => {
+const Conversation = ({ message }: any) => {
   return (
     <div>
-<div className="chat chat-start">
-  <div className="chat-bubble">
-    Hello How can I help you today?
-    <br />
-    Hi
-  </div>
-</div>
-<div className="chat chat-end">
-  <div className="chat-bubble">You underestimate my power!</div>
-</div>
+      <div className="chat chat-start">
+        <div className="chat-bubble">
+          Hello How can I help you today?
+          <br />
+          Hi
+        </div>
+      </div>
+      <div className="chat chat-end">
+        <div className="chat-bubble">{message.text}</div>
+      </div>
     </div>
   );
-}
+};
 export default Conversation;
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,37 +1,53 @@
+'use client';
 import Conversation from "./components/chatbubble";
 import Navbar from "./components/navbar";
+import React, { useState } from "react";
 
 export default function HomePage() {
+  const [message, setMessage] = useState("");
+  const sendMessage = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (message.trim() === "") {
+      return;
+    }
+    setMessage("");
+  };
+
   return (
-    <div className="relative">
+    <div className="relative min-h-screen flex flex-col">
       <header>
         <Navbar />
       </header>
+      <div className="h-5"></div>
       <Conversation />
-      <div className="h-100 w-100% bg-[#f2f0f7]">
+      <div className="absolute bottom-5 w-full bg-[#f2f0f7]">
         <div className="flex justify-center">
-          <input
-            type="text"
-            placeholder="Type here"
-            className="input input-bordered w-full max-w-lg"
-          />
-          <button className="btn btn-sm m-2">
-            send{" "}
-            <svg
-              // xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
-              />
-            </svg>
-          </button>
+          <form onSubmit={sendMessage}>
+            <input
+              type="text"
+              placeholder="Type here"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              className="input input-bordered w-full max-w-5xl"
+            />
+            <button type="submit" className="btn btn-sm m-2">
+              send{" "}
+              <svg
+                // xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+                />
+              </svg>
+            </button>
+          </form>
         </div>
       </div>
     </div>
